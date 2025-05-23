921ba5d0c2205f07d84bd5fe17b1ae7e1c7893c3
Okoro-Wisdom <ebosetazewisdom.okoro@istitutoagnelli.it>
2025-05-23 12:49:39 +0200

    Upgrade

--- docs/_app/immutable/entry/start.BJPzjo_r.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Bp615mvL.js";export{o as load_css,r as start};
---
+++ docs/_app/immutable/nodes/3.BkeQYLY-.js
@@ -0,0 +1,1 @@
+import{f as p,a as r}from"../chunks/CIbTKPDu.js";import"../chunks/Cc1Wc1vR.js";var t=p("<h1>About page</h1>");function n(o){var a=t();r(o,a)}export{n as component};
